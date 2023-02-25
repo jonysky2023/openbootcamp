2612fc5cd8b2f8c6133ad4c4fe2fa5c64cb9d3f6
jonysky2023 <122832864+jonysky2023@users.noreply.github.com>
2023-02-25 18:43:49 +0100

    Add files via upload

---
+++ Ejercicio_9_1.py
@@ -0,0 +1,15 @@
+paises = []
+
+def añadirpaises():
+    pais = input("Añada un pais:")
+    paises.append(pais)
+
+añadirpaises()
+preguntarañadir = input("desea agregar mas paises? Y/N")
+
+while preguntarañadir == "Y" or preguntarañadir == "y":
+    añadirpaises()
+    preguntarañadir = input("desea agregar mas paises? Y/N")
+
+unicos = list(set(paises))
+print(unicos)
